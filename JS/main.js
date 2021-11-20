b6a70e698a8ec3cc66828e5d8845e421946d5685
paulinaarango <93437820+paulinaarango@users.noreply.github.com>
2021-11-20 00:38:44 -0500

    Initial commit

---
+++ JS/main.js
@@ -0,0 +1,93 @@
+window.addEventListener('scroll',function(){
+    let animacion = this.document.getElementById('animado');
+    let posicionObj1 = animacion.getBoundingClientRect().top;
+    console.log(posicionObj1);
+    let tamañoDePantalla = window.innerHeight/2;
+
+    if(posicionObj1 < tamañoDePantalla){
+        animacion.style.animation = 'mover 1s ease-out'
+    }
+})
+
+window.addEventListener('scroll',function(){
+    let animacion = this.document.getElementById('animado1');
+    let posicionObj1 = animacion.getBoundingClientRect().top;
+    console.log(posicionObj1);
+    let tamañoDePantalla = window.innerHeight/2;
+
+    if(posicionObj1 < tamañoDePantalla){
+        animacion.style.animation = 'mover 1s ease-out'
+    }
+})
+
+window.addEventListener('scroll',function(){
+    let animacion = this.document.getElementById('animado2');
+    let posicionObj1 = animacion.getBoundingClientRect().top;
+    console.log(posicionObj1);
+    let tamañoDePantalla = window.innerHeight/2;
+
+    if(posicionObj1 < tamañoDePantalla){
+        animacion.style.animation = 'mover 1s ease-out'
+    }
+})
+
+window.addEventListener('scroll',function(){
+    let animacion = this.document.getElementById('animado3');
+    let posicionObj1 = animacion.getBoundingClientRect().top;
+    console.log(posicionObj1);
+    let tamañoDePantalla = window.innerHeight/2;
+
+    if(posicionObj1 < tamañoDePantalla){
+        animacion.style.animation = 'mover 1s ease-out'
+    }
+})
+window.addEventListener('scroll',function(){
+    let animacion = this.document.getElementById('img1');
+    let posicionObj1 = animacion.getBoundingClientRect().top;
+    console.log(posicionObj1);
+    let tamañoDePantalla = window.innerHeight/2;
+
+    if(posicionObj1 < tamañoDePantalla){
+        animacion.style.animation = 'mover 1s ease-out'
+    }
+})
+window.addEventListener('scroll',function(){
+    let animacion = this.document.getElementById('img2');
+    let posicionObj1 = animacion.getBoundingClientRect().top;
+    console.log(posicionObj1);
+    let tamañoDePantalla = window.innerHeight/2;
+
+    if(posicionObj1 < tamañoDePantalla){
+        animacion.style.animation = 'mover 1s ease-out'
+    }
+})
+window.addEventListener('scroll',function(){
+    let animacion = this.document.getElementById('img3');
+    let posicionObj1 = animacion.getBoundingClientRect().top;
+    console.log(posicionObj1);
+    let tamañoDePantalla = window.innerHeight/2;
+
+    if(posicionObj1 < tamañoDePantalla){
+        animacion.style.animation = 'mover 1s ease-out'
+    }
+})
+window.addEventListener('scroll',function(){
+    let animacion = this.document.getElementById('img4');
+    let posicionObj1 = animacion.getBoundingClientRect().top;
+    console.log(posicionObj1);
+    let tamañoDePantalla = window.innerHeight/2;
+
+    if(posicionObj1 < tamañoDePantalla){
+        animacion.style.animation = 'mover 1s ease-out'
+    }
+})
+window.addEventListener('scroll',function(){
+    let animacion = this.document.getElementById('img5');
+    let posicionObj1 = animacion.getBoundingClientRect().top;
+    console.log(posicionObj1);
+    let tamañoDePantalla = window.innerHeight/2;
+
+    if(posicionObj1 < tamañoDePantalla){
+        animacion.style.animation = 'mover 1s ease-out'
+    }
+})
